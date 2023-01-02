7a002ae91e5a19dcd246de1c9d177f91a0a93ed5
jhbai <brianbai0101@gmail.com>
2023-01-02 14:14:14 +0800

    Modified

--- Plot.py
+++ Plot.py
@@ -1,3 +1,10 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import matplotlib.animation as animation
+from IPython.display import HTML
+import matplotlib.pyplot as plt
+
 class draw:
     def __init__(self, img_flag):
         if not img_flag:
@@ -13,23 +20,20 @@ class draw:
             self.plots.append(Input)
 
     def show(self):
-        fig = 
+        fig = plt.figure(figsize=(8,8))
         if self.flag:
+            plt.axis("off")
             ani = animation.ArtistAnimation(fig, self.imgs, interval=1000, repeat_delay=1000, blit=True)
         else:
             ani = animation.ArtistAnimation(fig, self.plots, interval=1000, repeat_delay=1000, blit=True)
-        
         HTML(ani.to_jshtml())
 
     def help(self):
         if not self.flag:
-            print('plt.figure(figsize=(8,8))')
             temp = 'self.plots.append(plt.plot(pred.reshape(-1,).cpu().detach(), color = '
             temp += '\'red\', label = \'prediction\', animated = True)[0])'
             print(temp)
         else:
-            print('plt.figure(figsize=(8,8))')
-            print('plt.axis(\"off\")')
             temp = '[plt.imshow(np.transpose(img,(1,2,0)), animated=True)]'
             print(temp)
 
